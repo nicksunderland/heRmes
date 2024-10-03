0f261bcc540d89eaa8470957757fca36a438deb9
Nicholas Sunderland <nicholas.p.sunderland@gmail.com>
2024-10-03 10:10:30 +0100

    base QC script structure

--- scripts/gwas_qc.R
+++ scripts/gwas_qc.R
@@ -4,11 +4,9 @@
 #
 #  GWAS QC script
 #
-#  Command line options:
-#  -
-#  -
+#  Command line options: run this script with the -h/--help flag to see options
 #
-#
+
 
 #=============================================================================
 # required packages
@@ -24,8 +22,10 @@ suppressPackageStartupMessages(library(data.table))
 cli_h2("Parsing arguments")
 
 parser <- ArgumentParser()
+# basic controls
 parser$add_argument("-v", "--verbose",    action="store_true",  default=TRUE,     help="Print extra output [default]")
 parser$add_argument("-q", "--quietly",    action="store_false", dest="verbose",   help="Print little output")
+# file paths and file handling
 parser$add_argument("-g", "--gwas",       action="store",       type="character", help="GWAS file path")
 parser$add_argument("-r", "--ref",        action="store",       type="character", help="Reference file path")
 parser$add_argument("-o", "--output",     action="store",       type="character", help="Output file path")
@@ -47,21 +47,26 @@ parser$add_argument("-r_bp",  "--ref_bp",  action="store", type="character", hel
 parser$add_argument("-r_ea",  "--ref_ea",  action="store", type="character", help="Reference effect allele column name in the GWAS file", default = "ea")
 parser$add_argument("-r_oa",  "--ref_oa",  action="store", type="character", help="Reference other allele column name in the GWAS file", default = "oa")
 parser$add_argument("-r_eaf", "--ref_eaf", action="store", type="character", help="Reference effect allele frequency column name in the GWAS file", default = "eaf")
-
+# QC parameters
+parser$add_argument("-gc", "--genomic_control", action="store_true", default=FALSE, help="Apply genomic control for adjusting study results [default=FALSE]")
+parser$add_argument("-indel", "--indel_alleles", action="store_true", default=TRUE, help="Reatin indel alleles [default=TRUE]")
+# parse CLI arguments
 args <- parser$parse_args()
 
-
+# print the arguments requested to the console
 for (i in seq_along(args)) {
   cli_text("{.strong {names(args)[i]}}:          {.val {args[[i]]}}")
 }
 
 
+#=============================================================================
+# start QC run
+#=============================================================================
 cli_h1("Running GWAS quality control")
-#cli_progress_bar("Running", total = 100)
-#cli_progress_update()
+
 
 #=============================================================================
-# check files
+# basic check files
 #=============================================================================
 cli_progress_step("checking file paths and columns names")
 
@@ -69,7 +74,6 @@ cli_progress_step("checking file paths and columns names")
 if (is.null(args$gwas) || !file.exists(args$gwas) || file.access(args$gwas, 4) != 0) {
   cli_abort("GWAS file path `{.file {args$gwas}}` does not exist or is not readable")
 }
-
 
 # Check GWAS file column names all present
 gwas_header <- fread(args$gwas, nrows = 0)
@@ -85,12 +89,10 @@ if (!all(gwas_cols)) {
   cli_abort("Columns not all found in GWAS file columns [{names(gwas_header)}]")
 }
 
-
 # Check reference file path provided and that we can read from in
 if (is.null(args$ref) || !file.exists(args$ref) || file.access(args$ref, 4) != 0) {
   cli_abort("Reference file path `{.file {args$reference}}` does not exist or is not readable")
 }
-
 
 # Check GWAS file column names all present
 ref_header <- fread(args$ref, nrows = 0)
@@ -106,7 +108,6 @@ if (!all(ref_cols)) {
   cli_abort("Columns not all found in reference file columns [{names(ref_header)}]")
 }
 
-
 # Check output file path provided and that we can write to it / overwrite it
 if (is.null(args$output) || (file.access(args$output, 2) != 0 && args$overwrite)) {
   cli_abort("Output file path `{.file {args$output}}` not provided or does not have write permission")
@@ -118,30 +119,77 @@ if (is.null(args$output) || (file.access(args$output, 2) != 0 && args$overwrite)
 #=============================================================================
 # read data
 #=============================================================================
-
 cli_progress_step("Reading GWAS")
-Sys.sleep(2)
+Sys.sleep(1)
 cli_progress_step("Reading reference")
-Sys.sleep(2)
+Sys.sleep(1)
+
 
 #=============================================================================
 # check column data
 #=============================================================================
-
 cli_progress_step("Pre-QC summary")
+Sys.sleep(1)
+
+
+#=============================================================================
+# data fixes
+#=============================================================================
+cli_progress_step("Data standardisation")
+Sys.sleep(1)
+
+
+#=============================================================================
+# apply filter
+#=============================================================================
+cli_progress_step("Cleaning data")
+Sys.sleep(1)
 
 
 #=============================================================================
 # harmonise alleles
 #=============================================================================
-cli_progress_step("Pre-QC summary")
-
-
+cli_progress_step("Reference harmonisation")
+Sys.sleep(1)
 
 
 #=============================================================================
-# EAF plot
+# allele frequency analysis
 #=============================================================================
-cli_progress_step("Comparing GWAS and reference allele frequency")
+cli_progress_step("Analysing allele frequency")
+Sys.sleep(1)
 
+
+#=============================================================================
+# PZ plot
+#=============================================================================
+cli_progress_step("Creating PZ plot")
+Sys.sleep(1)
+
+
+#=============================================================================
+# population stratification analysis 1
+#=============================================================================
+cli_progress_step("Calculating lambda-GC")
+Sys.sleep(1)
+
+
+#=============================================================================
+# population stratification analysis 2
+#=============================================================================
+cli_progress_step("Running LDSC")
+Sys.sleep(1)
+
+
+#=============================================================================
+# save
+#=============================================================================
+cli_progress_step("File saved to {.file {args$output}}")
+Sys.sleep(1)
+
+
+#=============================================================================
+# finished
+#=============================================================================
+cli_process_done()
 

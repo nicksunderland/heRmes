701c3c4f5ef2c126c0e7121757e706147d1c4367
Nicholas Sunderland <nicholas.p.sunderland@gmail.com>
2024-10-24 00:07:16 +0100

    testing html rendering

---
+++ scripts/rmd_test.sh
@@ -0,0 +1,5 @@
+
+
+rmd_file="/Users/xx20081/git/heRmes/scripts/gwas_qc.Rmd"
+gwas_qc_dir="/Users/xx20081/Desktop/qc_tests"
+Rscript -e "rmarkdown::render('$rmd_file', params = list(fig_dir = '$gwas_qc_dir'), output_dir = '$gwas_qc_dir')"
